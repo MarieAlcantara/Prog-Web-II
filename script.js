f916abcae9a58b5ac330024b392edfdb9a8a26bb
MarieAlcantara <101595804+MarieAlcantara@users.noreply.github.com>
2022-08-22 15:23:38 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,43 +1,43 @@
-document.addEventListener("DOMContentLoaded", insereSumario)
+document.addEventListener("DOMContentLoaded", insereSumario) 
 
-function insereSumario () {
-  const ol = document.crateElement('ol')
-  ol.id = 'sumário'
-  document.body.prepend(ol)
-  const secoes = document.querySelectorAll('h2')
-  for(let i-0; i<secoes.length; i++){
-      const li = criarItem('#sec${i + 1}', secoes[i].textContent)
-      ol.appendChild(li)
-      alterarH2(secoes[i], i+1 )
-     }   
+function insereSumario(){
+	const ol = document.createElement('ol')
+	ol.id = 'sumário'
+	document.body.prepend(ol)
+	const secoes = document.querySelectorAll('h2')
+	for(let i=0; i<secoes.length; i++) {
+		const li = criarItem(`#sec${i+1}`, secoes[i].textContent)
+		ol.appendChild(li) 
+		alterarH2(secoes[i], i+1) 
+	}
 }
 
-function alterarH2(elem, secnum) {
-  inserirNumero(elem, secnum)
-  elem.id = 'sec${secnum}'
-  const voltar = criarLink('#', 'voltar')
-  inserirDepois(elem, voltar)
+function alterarH2(elem, secnum){ 
+	inserirNumero(elem, secnum)
+	elem.id = `sec${secnum}`
+	const voltar = criarLink('#', 'voltar') 
+	inserirDepois(elem, voltar)
 }
 
-function criaritem(href, text) {
-  const li = document.createElement('li')
-  const linque = criarLink(href, texto)
-  li.appendChild(linque)
-  return li
+function criarItem(href, texto){
+	const li = document.createElement('li')
+	const linque = criarLink(href,texto)
+	li.appendChild(linque)
+	return li
 }
 
 function inserirNumero(elem, numero) {
-  elem.textContent = '${numero}. ${elem.textContent}'
+	elem.textContent = `${numero}. ${elem.textContent}`
 }
 
 function criarLink(href, texto) {
-  const linque = document.createElement('a')
-  linque.setAttribute('href', href)
-  linque.textContent = texto
-  return linque 
+	const linque = document.createElement('a') 
+	linque.setAttribute('href', href) 
+	linque.textContent = texto
+	return linque 
 }
-
+	
 function inserirDepois(de, elem) {
-  const proximoIrmao = de.nextElementSibiling;
-  de.parentElement.insertBefore(elem, proximoIrmao)
+	const proximoIrmao = de.nextElementSibling;
+	de.parentElement.insertBefore(elem,proximoIrmao)
 }
